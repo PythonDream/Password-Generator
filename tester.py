14be955beff4445dd7a3f05c5c9af69d840f7361
PythonDream <Ghostetr@gmail.com>
2017-06-02 13:29:41 +0200

    testing some grounds here

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 from classes.password import PasswordGenerator
 test = PasswordGenerator()
 
-#for _ in range(20):
+# for _ in range(20):
 print(test.random_password('nls', 5))
 print(test.random_password('nls', 5))
 print(test.random_upper_lower('shuffle_letters', 'Alexander Lausten'))
